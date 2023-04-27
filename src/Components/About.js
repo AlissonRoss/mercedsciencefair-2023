8e7d84d5998b766f8be220b1c94190fd53153c5a
Alisson Ross <alisson@alisson.me>
2023-04-26 21:34:19 -0700

    Made lots of responsiveness and css fixes

--- src/Components/About.js
+++ src/Components/About.js
@@ -4,17 +4,19 @@ function About(){
     return(
         <div className="About">
             <h2 className="About-Description">
-                Registration is open to all Undergrad and High school students 
-                <br/>in the central valley!
+            &nbsp;Join in the excitement of STEM and enter to win awesome prizes in the Merced College annual Science Fair.  This year’s task is to invent something new and cool in the pursuance of STEM.  It can be any invention, 
+                but projects that involve some type of programming can win in a special category
+                &nbsp;Registration is open to all Undergrad and High school students 
+                in the central valley!
                 <br/>
-                <Button variant="contained" style={{margin: "2em"}}href="https://forms.gle/WQNv3XrKPyRyh86a7">
+                <Button variant="contained" className="About-Button" href="https://forms.gle/WQNv3XrKPyRyh86a7">
                     Registration Form
                 </Button>
                 <br/>
-                This website is under construction by the members of the 
+                &nbsp;This website is under construction by the members of the 
                 <br/>CAHSI Merced College computer science club
             </h2>
-            <video loop width="450" className="Video" autoPlay src={Video} type="video/mp4"/>
+            <video loop className="Video" autoPlay src={Video} type="video/mp4"/>
         </div>
         
     );
