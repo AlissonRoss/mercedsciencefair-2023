bff5f360ad07b37e3d1a6657acfa9f8b40aa1537
unknown <you@example.com>
2023-04-30 18:55:33 -0700

    feat(layout and mobile friendly

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Landing from './Components/Landing.js';
 import About from './Components/About.js';
+import NavBar from './Components/NavBar.js'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+        <NavBar/>
       <header className="App-header">
         <Landing/>
       </header>
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,5 +1,67 @@
-function NavBar(){
-    return(
-        <h1>This is the navbar component</h1>
+import { Link, IconButton } from "@mui/material";
+import { Icon } from '@mui/material';
+import Logo from '../Assets/Cahsi-logo1.png';
+import { useEffect, useState } from "react";
+
+
+function NavBar() {
+    const [showSideBar, setShowSideBar] = useState(false)
+
+
+    useEffect(() => { // controls mobile sidebar
+        function reSize() {
+            if (window.innerWidth > 524 && showSideBar === true) {
+                setShowSideBar(false);
+            }
+        }
+        window.addEventListener('resize', reSize)
+
+        return () => { //clean up event listener, removes event listener
+            window.removeEventListener('resize', reSize)
+        }
+    }, [showSideBar])
+
+    function showSide() {
+        setShowSideBar(bool => !bool)
+    }
+    return (
+        <>
+            <nav className="navbar-container" id='go-home'>
+                <div className="logo-nav">
+
+                    <img className="logo-img" src={Logo} alt="CAHSI logo" />
+                </div>
+                <ul className="links-ul">
+                    <Link className="links-li" href="#about">About</Link>
+                    <Link className="links-li" href="#prizes">Prizes</Link>
+                    <Link className="links-li" href="#footer">Contacts</Link>
+                </ul>
+
+                {showSideBar &&
+                    <div className="sidebar-container">
+                        <ul className="sidebar-ul">
+                            <Link className="sidebar-li" href="#about">About</Link>
+                            <Link className="sidebar-li" href="#prizes">Prizes</Link>
+                            <Link className="sidebar-li" href="#footer">Contacts</Link>
+                        </ul>
+                    </div>
+
+                }
+                <IconButton //component from MUI
+                    className="hamburger-icon"
+                    edge="start"
+                    aria-label="open drawer"
+                    sx={{ mr: 1, display: "flex", color: "white" }}
+                >
+                    <Icon  //component from MUI 
+                        onClick={showSide}>
+                        <svg viewBox="0 0 24 24">
+                            <path fill="currentColor" d="M3 18h18v-2H3v2zM3 13h18v-2H3v2zM3 6v2h18V6H3z" />
+                        </svg>/1
+                    </Icon>
+                </IconButton>
+
+            </nav>
+        </>
     );
 } export default NavBar;
